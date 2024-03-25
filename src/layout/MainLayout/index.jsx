ae88b63bde0f56451eafa2450f487f4e0ed506a4
Sang Cao <thanhsang0312@gmail.com>
2024-03-25 20:02:32 +0700

    update

--- src/layout/MainLayout/index.jsx
+++ src/layout/MainLayout/index.jsx
@@ -1,27 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import PageLoading from "../../components/PageLoading";
-import Header from "../../components/Header";
-import Navbar from "../../components/Navbar";
-import Overlay from "../../components/Overlay";
-import Footer from "../../components/Footer";
-import LoginModal from "../../components/LoginModal";
-import { Outlet } from "react-router-dom";
-
-const MainLayout = ({ children }) => {
-  return (
-    <>
-      <PageLoading />
-      <Header />
-      <Navbar />
-      <Overlay />
-
-      <Outlet />
-
-      <Footer />
-      <LoginModal />
-    </>
-=======
 import { Outlet } from "react-router-dom";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
@@ -46,7 +23,6 @@ const MainLayout = ({ children }) => {
         <LoginModal />
       </AuthContextProvider>
     </MainContextProvider>
->>>>>>> 5597a8e (update)
   );
 };
 
