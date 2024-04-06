882459c4bce9bcb7e5f5e7d9c93444059726c991
Sang Cao <thanhsang0312@gmail.com>
2024-04-06 10:36:11 +0700

    handleCloseMenu & scrollTop after naviagte

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,32 +1,55 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useMainContext } from "../../context/MainContext";
 
 const Navbar = () => {
+  const { handleShowNavbar, isShowNavbar } = useMainContext();
+
   return (
     <nav className="navbar">
       <ul className="navbar__main">
         <li className="navbar__link">
-          <NavLink to={"/"} className="navbar__item">
+          <NavLink
+            to={"/"}
+            className="navbar__item"
+            onClick={() => handleShowNavbar(!isShowNavbar)}
+          >
             Trang chủ
           </NavLink>
         </li>
         <li className="navbar__link">
-          <NavLink to="/about" className="navbar__item">
+          <NavLink
+            to="/about"
+            className="navbar__item"
+            onClick={() => handleShowNavbar(!isShowNavbar)}
+          >
             Về CFD Circle
           </NavLink>
         </li>
         <li className="navbar__link">
-          <NavLink to="/course" className="navbar__item">
+          <NavLink
+            to="/course"
+            className="navbar__item"
+            onClick={() => handleShowNavbar(!isShowNavbar)}
+          >
             Khóa học
           </NavLink>
         </li>
         <li className="navbar__link">
-          <NavLink to="/blog" className="navbar__item">
+          <NavLink
+            to="/blog"
+            className="navbar__item"
+            onClick={() => handleShowNavbar(!isShowNavbar)}
+          >
             Bài viết
           </NavLink>
         </li>
         <li className="navbar__link">
-          <NavLink to="/contact" className="navbar__item">
+          <NavLink
+            to="/contact"
+            className="navbar__item"
+            onClick={() => handleShowNavbar(!isShowNavbar)}
+          >
             Liên hệ
           </NavLink>
         </li>
--- src/context/MainContext.jsx
+++ src/context/MainContext.jsx
@@ -1,14 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useContext } from "react";
+import { useLocation } from "react-router-dom";
+import { scrollTop } from "../utils/scrollTop";
 
 const MainContext = createContext({});
 
 const MainContextProvider = ({ children }) => {
   const [isShowNavbar, setIsShowNavbar] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    handleShowNavbar(false);
+    const myTimeOut = setTimeout(() => {
+      scrollTop();
+    }, 100);
+    return () => {
+      clearTimeout(myTimeOut);
+    };
+  }, [pathname]);
 
   const handleShowNavbar = (isShow) => {
     setIsShowNavbar(isShow);
   };
+
   return (
     <MainContext.Provider value={{ isShowNavbar, handleShowNavbar }}>
       {children}
